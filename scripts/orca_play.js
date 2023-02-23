e2ccf119246a51551c0524f77646139eafcd1d34
Karsten <kjuschus@freenet.de>
2023-02-23 23:29:13 +0100

    Added command for local output

--- scripts/orca_play.js
+++ scripts/orca_play.js
@@ -1,5 +1,7 @@
 'use strict'
 
+/* global library */
+
 function OrcaPlay (client) {
     this.el = document.createElement('div')
   
@@ -69,6 +71,41 @@ function OrcaPlay (client) {
           const vel = msg.data[2]
           this.mixer.tweak(ch, knob, vel)
         }
-    }    
+    }  
+    
+    library['/'] = function OperatorToneJS (orca, x, y, passive) {
+      Operator.call(this, orca, x, y, '/', true)
+    
+      this.name = 'ToneJS'
+      this.info = 'Sends to ToneJS'
+      this.ports.channel = { x: 1, y: 0 }
+      this.ports.octave = { x: 2, y: 0, clamp: { min: 0, max: 8 } }
+      this.ports.note = { x: 3, y: 0 }
+      this.ports.velocity = { x: 4, y: 0, default: 'f', clamp: { min: 0, max: 16 } }
+      this.ports.length = { x: 5, y: 0, default: '1', clamp: { min: 0, max: 32 } }
+    
+      this.operation = function (force = false) {
+        if (!this.hasNeighbor('*') && force === false) { return }
+        if (this.listen(this.ports.channel) === '.') { return }
+        if (this.listen(this.ports.octave) === '.') { return }
+        if (this.listen(this.ports.note) === '.') { return }
+        if (!isNaN(this.listen(this.ports.note))) { return }
+    
+        const channel = this.listen(this.ports.channel, true)
+        if (channel > 15) { return }
+        const octave = this.listen(this.ports.octave, true)
+        const note = this.listen(this.ports.note)
+        const velocity = this.listen(this.ports.velocity, true)
+        const length = this.listen(this.ports.length, true)
+    
+        client.io.midi.push(channel, octave, note, velocity, length)
+    
+        if (force === true) {
+          client.io.midi.run()
+        }
+    
+        this.draw = false
+      }
+    }
 }
   
