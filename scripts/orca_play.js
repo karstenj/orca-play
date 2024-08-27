a4cae0b3e6e822c32d7ead6efcca43e3e163d9d3
zzandy <zzandy@gmail.com>
2024-08-27 13:07:37 +0200

    make guide orca-native

--- scripts/orca_play.js
+++ scripts/orca_play.js
@@ -1,149 +1,138 @@
-import {orcaExamples} from './examples.js'
+import { orcaExamples } from './examples.js'
 import Share from './share.js'
 import IOWrapper from './io_wrapper.js'
 'use strict'
 
-
-function OrcaPlay () {
+function OrcaPlay() {
   const { orcaClient, Operator } = document.getElementById('orca-iframe').contentWindow;
   const { pilotClient } = document.getElementById('pilot-iframe').contentWindow;
 
-
   this.orcaClient = orcaClient
-  this.orcaClient.toggleGuide(false);    
   this.pilotClient = pilotClient
   this.io = new IOWrapper(this)
   this.share = new Share()
   this.guide = false
   this.pilot = true
+
+  const isMac = navigator.platform.match(/^(?:Mac|iP)/)
+  const modifierKey = isMac ? '⌘' : 'Ctrl'
+  const alpOptKey = isMac ? '⌥' : 'Alt'
+  const shiftKey = isMac ? '⇧' : 'Shift'
   const orcaPlay = this
+  const orca = orcaPlay.orcaClient;
 
   this.install = (host = document.body) => {
     this.addAccels()
-    const infoClose = document.getElementById('info-close');      
-    const infoContainer = document.getElementById('info-container');     
-    const infoContent = document.getElementById('info-content'); 
-    // add close handler
-    infoClose.addEventListener('click', () => {
-      this.toggleGuide()
-    })
-    // render guide
-    this.renderGuide(infoContent)
-    // render accelerators
-    this.renderAccels(infoContent)
     // install IO
     this.io.install(host)
-    // hide orca guide
-    this.toggleGuide()
     // add key handler for orca accels
-    host.addEventListener('keydown', this.orcaClient.acels.onKeyDown, false)
-    host.addEventListener('keyup', this.orcaClient.acels.onKeyUp, false)  
+    host.addEventListener('keydown', orca.acels.onKeyDown, false)
+    host.addEventListener('keyup', orca.acels.onKeyUp, false)
     // add theme element
-    host.appendChild(this.orcaClient.theme.el)
+    host.appendChild(orca.theme.el)
     // read code from URL
     this.share.initCode().then((code) => {
       if (code) {
-        this.orcaClient.whenOpen(undefined, code)
+        orca.whenOpen(undefined, code)
         this.toggleGuide()
-        this.orcaClient.cursor.select(0, 0, 0, 0)
+        orca.cursor.select(0, 0, 0, 0)
       }
     })
     // set cursor
     // add pilot command handler
-    this.orcaClient.commander.actives["note"] = (p) => {
+    orca.commander.actives["note"] = (p) => {
       this.pilotNote(p.str)
     }
-    this.orcaClient.commander.actives["not"] = (p) => {
+    orca.commander.actives["not"] = (p) => {
       this.pilotNote(p.str)
     }
-    this.orcaClient.commander.actives["effect"] = (p) => {
+    orca.commander.actives["effect"] = (p) => {
       this.pilotEffect(p.str)
     }
-    this.orcaClient.commander.actives["eff"] = (p) => {
+    orca.commander.actives["eff"] = (p) => {
       this.pilotEffect(p.str)
+    }
+
+    this.guideContent = ['Operators'];
+    Object.entries(orca.library)
+      .forEach(([key, op]) => {
+        if (!isNaN(key)) return;
+        this.guideContent.push([key, new op().info]);
+      });
+
+    const cats = orca.acels.sort();
+    for (const cat in cats) {
+      this.guideContent.push(cat);
+      for (const item of cats[cat]) {
+        if (item.accelerator) {
+          this.guideContent.push([item.accelerator
+            .replace(/CmdOrCtrl/, modifierKey)
+            .replace(/Alt\+/i, `${alpOptKey}+`)
+            .replace(/Shift\+/i, `${shiftKey}+`)
+            .replace('ArrowUp', '↑')
+            .replace('ArrowRight', '→')
+            .replace('ArrowDown', '↓')
+            .replace('ArrowLeft', '←')
+
+
+            , item.name]);
+        }
+      }
+    }
+
+    orca.drawGuide = () => {
+      if (orca.guide !== true) { return }
+
+      orca.write('ORCΛ PLΛY', 2, 0, 100, 4)
+      orca.write('A combination of the famous Orca esoteric programming language', 2, 1, 100, 10)
+      orca.write('and the Pilot synthesizer that allows to run Orca in a browser', 2, 2, 100, 10)
+      orca.write(`without additional setup. Press ${modifierKey}+R for an example.`, 2, 3, 100, 10)
+
+      const lines = orca.orca.h - 7
+      let n = 0;
+      let width = 1;
+      const col = 32;
+
+      for (const line of this.guideContent) {
+        const x = 2 + 32 * ((n / lines) | 0)
+        const y = 5 + (n % lines)
+
+        if (line instanceof Array) {
+          orca.write(line[0], x, y, width, 3)
+          orca.write(line[1], x + line[0].length + 1, y, col - width - 1, 10)
+        }
+        else if (line.length < 15) { // category title
+          orca.write(line, x, y, col, 8)
+          let i = 1;
+          width = -Infinity;
+          while (n + i < this.guideContent.length && this.guideContent[n + i] instanceof Array) {
+            width = Math.max(width, this.guideContent[n + i][0].length);
+            ++i;
+          }
+        }
+        else { // any text
+          orca.write(line, x, y, col, 3)
+        }
+
+        ++n;
+      }
     }
   }
 
-  this.start = (bpm = 120) => {
+  this.start = () => {
     console.info('OrcaPlay', 'Starting..')
     this.io.start()
   }
 
-  this.renderGuide = (infoContent) => {
-    const templ = document.querySelector("#info-table-template")
-    let clone = templ.content.cloneNode(true);
-    const templ_row = document.querySelector("#info-table-template-row")    
-    clone.querySelector("h2").textContent = 'Operators'
-    let th = clone.querySelectorAll("th");
-    th[0].textContent = 'Operator'
-    th[1].textContent = 'Description'
-    const cats = this.orcaClient.acels.sort()
-    let tbody = clone.querySelector("tbody");
-    const operators = Object.keys(this.orcaClient.library).filter((val) => { return isNaN(val) })
-    for (const id in operators) {
-      const key = operators[id]
-      const oper = new this.orcaClient.library[key]()
-      const text = oper.info
-      let clone_row = templ_row.content.cloneNode(true);
-      let td = clone_row.querySelectorAll("td");
-      td[0].textContent = key
-      td[1].textContent = text
-      tbody.appendChild(clone_row)
-    }
-    infoContent.appendChild(clone)
-  }
-
-  this.renderAccels = (infoContent) => {
-    const templ = document.querySelector("#info-table-template")
-    let clone = templ.content.cloneNode(true);
-    clone.querySelector("h2").textContent = 'Accelerators'
-    let th = clone.querySelectorAll("th");
-    th[0].textContent = 'Key'
-    th[1].textContent = 'Function'
-    const cats = this.orcaClient.acels.sort()
-    const templ_row = document.querySelector("#info-table-template-row")    
-    const templ_row_group = document.querySelector("#info-table-template-row-group")    
-    let tbody = clone.querySelector("tbody");
-    for (const cat in cats) {
-      let clone_row = templ_row_group.content.cloneNode(true);
-      let td = clone_row.querySelector("td");
-      td.textContent = cat
-      tbody.appendChild(clone_row)
-      for (const item of cats[cat]) {
-        if (item.accelerator) {
-          let clone_row = templ_row.content.cloneNode(true);
-          let td = clone_row.querySelectorAll("td");
-          td[0].textContent = item.accelerator.replace('`', 'tilde')
-          td[1].textContent = item.name
-          tbody.appendChild(clone_row)
-        }
-      }
-    }
-    infoContent.appendChild(clone)
-  }
-
   this.addAccels = () => {
-    this.orcaClient.acels.set('View', 'Toggle Guide', 'CmdOrCtrl+G', () => { this.toggleGuide() })      
-    this.orcaClient.acels.set('View', 'Hide Pilot', 'CmdOrCtrl+H', () => { this.hidePilot() })
-    this.orcaClient.acels.set('File', 'Load Random Example', 'CmdOrCtrl+R', () => { this.randomExample() })
-    this.orcaClient.acels.set('File', 'Share Link', 'CmdOrCtrl+#', () => { this.shareLink() })
-  }
-
-  this.toggleGuide = () => {
-    const infoContainer = document.getElementById('info-container');
-    if (this.guide) {
-      infoContainer.classList.add('hidden');
-      infoContainer.classList.remove('flex');
-    } else {
-      infoContainer.classList.remove('hidden');
-      infoContainer.classList.add('flex');        
-    }
-    this.guide = !this.guide
+    orca.acels.set('View', 'Hide Pilot', 'CmdOrCtrl+H', () => { this.hidePilot() })
+    orca.acels.set('File', 'Load Random Example', 'CmdOrCtrl+R', () => { this.randomExample() })
+    orca.acels.set('File', 'Share Link', 'CmdOrCtrl+#', () => { this.shareLink() })
   }
 
   this.hidePilot = () => {
-    const pilot = document.getElementById('pilot');     
-    const orca = document.getElementById('orca');   
+    const pilot = document.getElementById('pilot');
+    const orca = document.getElementById('orca');
     if (this.pilot) {
       pilot.classList.add('hidden')
       pilot.classList.remove('pilot_visible')
@@ -165,11 +154,11 @@ function OrcaPlay () {
     const keys = Object.keys(orcaExamples)
     const key = keys[Math.floor(Math.random() * keys.length)];
     console.log('Loading ' + key)
-    this.orcaClient.whenOpen(undefined, orcaExamples[key])
+    orca.whenOpen(undefined, orcaExamples[key])
   }
 
   this.shareLink = () => {
-    this.share.handleShare(`${this.orcaClient.orca}`)
+    this.share.handleShare(`${orca.orca}`)
   }
 
   this.pilotNote = (command) => {
@@ -180,12 +169,12 @@ function OrcaPlay () {
     this.pilotClient.mixer.run_effect(command)
   }
 
-  orcaClient.library[';'] = function OperatorPilotNote (orca, x, y, passive) {
+  orcaClient.library[';'] = function OperatorPilotNote(orca, x, y, passive) {
     Operator.call(this, orca, x, y, ';', true)
-  
+
     this.name = 'pilot synth'
     this.info = 'PILOT synth/sampler'
-  
+
     this.operation = function (force = false) {
       let msg = ''
       for (let x = 1; x <= 36; x++) {
@@ -194,24 +183,24 @@ function OrcaPlay () {
         if (g === '.') { break }
         msg += g
       }
-  
+
       if (msg.length < 3) { return }
       if (!this.hasNeighbor('*') && force === false) { return }
-  
+
       this.draw = false
       orcaPlay.io.io_pilot.push_note(msg)
-  
+
       if (force === true) {
         orcaPlay.io.io_pilot.run_note()
       }
     }
   }
-  orcaClient.library['='] = function OperatorPilotEffect (orca, x, y, passive) {
+  orcaClient.library['='] = function OperatorPilotEffect(orca, x, y, passive) {
     Operator.call(this, orca, x, y, ';', true)
-  
+
     this.name = 'pilot effect/drum'
     this.info = 'PILOT effect/drum'
-  
+
     this.operation = function (force = false) {
       let msg = ''
       for (let x = 1; x <= 36; x++) {
@@ -220,20 +209,20 @@ function OrcaPlay () {
         if (g === '.') { break }
         msg += g
       }
-  
+
       if (msg.length < 2) { return }
       if (!this.hasNeighbor('*') && force === false) { return }
-  
+
       this.draw = false
       orcaPlay.io.io_pilot.push_effect(msg)
-  
+
       if (force === true) {
         orcaPlay.io.io_pilot.run_effect()
       }
     }
   }
 }
-  
+
 window.onload = () => {
   const orcaPlay = new OrcaPlay()
   orcaPlay.install(document.body)
